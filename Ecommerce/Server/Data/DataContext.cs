8dede0540fd7b6a2329605fb8f88d181bf70eaef
A-Fog <A-Fogle1994@outlook.com>
2023-12-12 14:49:52 -0500

    Add project files.

---
+++ Ecommerce/Server/Data/DataContext.cs
@@ -0,0 +1,44 @@
+﻿namespace Ecommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+        new Product
+        {
+            Id = 1,
+            Title = "Pokemon Red, Blue and Yellow bundle",
+            Description = "Experience the very beginning as we go back to the first generation that started it all.",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Pok%C3%A9mon_Red_and_Blue_cover_art.webp/546px-Pok%C3%A9mon_Red_and_Blue_cover_art.webp.png",
+            Price = 80.00m
+        },
+        new Product
+        {
+            Id = 2,
+            Title = "Pokemon Gold and Silver bundle",
+            Description = "Experience the sequels that changed everything and perfected the formula for the biggest franchice in the early 2000s.",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/en/4/4c/Pok%C3%A9mon_box_art_-_Gold_Version.png",
+            Price = 299.99m
+        },
+        new Product
+        {
+            Id = 3,
+            Title = "Pokemon Ruby and Sapphire bundle",
+            Description = "The wild child of the ever growing family of triple pack games and the start of the advanced generation of poekmon games, with more features than you could possibly count!",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/be/PokemonRubySapphireBox.jpg",
+            Price = 479.95m
+        }
+
+                );
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Ecommerce/Server/Migrations/20231212192420_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "Experience the very beginning as we go back to the first generation that started it all.", "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Pok%C3%A9mon_Red_and_Blue_cover_art.webp/546px-Pok%C3%A9mon_Red_and_Blue_cover_art.webp.png", 80.00m, "Pokemon Red, Blue and Yellow bundle" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Experience the sequels that changed everything and perfected the formula for the biggest franchice in the early 2000s.", "https://upload.wikimedia.org/wikipedia/en/4/4c/Pok%C3%A9mon_box_art_-_Gold_Version.png", 299.99m, "Pokemon Gold and Silver bundle" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "The wild child of the ever growing family of triple pack games and the start of the advanced generation of poekmon games, with more features than you could possibly count!", "https://upload.wikimedia.org/wikipedia/en/b/be/PokemonRubySapphireBox.jpg", 479.95m, "Pokemon Ruby and Sapphire bundle" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
